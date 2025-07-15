4716497b5e6a4f85bfea7179d4f73c2eb9f8f79b
Vivek Kumar Giri <giriv4823@example.com>
2025-07-15 19:36:53 +0530

    Initial Setup

--- src/landing_page/products/ProductPage.js
+++ src/landing_page/products/ProductPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Hero from './Hero';
 import Leftimage from './Leftimage'; 
+import Rightimage from './Rightimage';
+import Universe from './Universe';
+import NavBar from '../Navbar';
+import Footer from '../Footer';
 
 function ProductPage() {
     return ( 
         <>
+        <NavBar />
             <Hero />
             <Leftimage />
+            <Rightimage />
+            <Universe />
             <h1>ProductPage</h1>
+        <Footer/>
         </>
     );
 }
